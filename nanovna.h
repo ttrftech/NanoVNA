47f48d83e4bdaf24039e73e0da525e04dac623c7
TT <edy555@gmail.com>
2019-09-23 12:43:39 +0900

    fix: omit drawing screen from usb thread

--- nanovna.h
+++ nanovna.h
@@ -236,7 +236,7 @@ void redraw_marker(int marker, int update_info);
 void trace_get_info(int t, char *buf, int len);
 void plot_into_index(float measured[2][101][2]);
 void force_set_markmap(void);
-void draw_all_cells(void);
+void draw_all(void);
 
 void draw_cal_status(void);
 
@@ -245,7 +245,13 @@ void markmap_all_markers(void);
 void marker_position(int m, int t, int *x, int *y);
 int search_nearest_index(int x, int y, int t);
 
-extern int8_t redraw_requested;
+extern uint16_t redraw_request;
+
+#define REDRAW_CELLS      (1<<0)
+#define REDRAW_FREQUENCY  (1<<1)
+#define REDRAW_CAL_STATUS (1<<2)
+#define REDRAW_MARKER     (1<<3)
+
 extern int16_t vbat;
 
 /*
